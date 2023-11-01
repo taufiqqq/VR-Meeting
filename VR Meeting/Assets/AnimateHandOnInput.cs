e76aed02f7df69670f77fa143ad0c1793f22f4d6
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2023-11-02 04:25:06 +0800

    Update movement/handtracking
    Created hand animation (pinching, grip)
    Add locomotion system to let user move
    Added snap turn functionality
    Implement gravity to the character

--- VR Meeting/Assets/AnimateHandOnInput.cs
+++ VR Meeting/Assets/AnimateHandOnInput.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AnimateHandOnInput : MonoBehaviour
 {
+
+    [SerializeField] private InputActionProperty pinchAnimationAction;
+    [SerializeField] private InputActionProperty gripAnimationAction;
+    [SerializeField] private Animator handAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class AnimateHandOnInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+        Debug.Log(triggerValue + " " + gripValue);
     }
 }
