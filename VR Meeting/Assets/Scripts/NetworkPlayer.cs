22bcdfa76d5564613f45340e57fde7f0c5e4fa60
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2023-11-13 16:16:35 +0800

    update
    implement lobby and relay system

---
+++ VR Meeting/Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    public Transform root;
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+
+    public Renderer[] meshToDisable;
+
+    // Start is called before the first frame update
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        if (IsOwner)
+        {
+            foreach(var item in meshToDisable)
+            {
+                item.enabled = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsOwner)
+        {
+            root.position = VRRigReferences.Singleton.root.position;
+            root.rotation = VRRigReferences.Singleton.root.rotation;
+
+            head.position = VRRigReferences.Singleton.head.position;
+            head.rotation = VRRigReferences.Singleton.head.rotation;
+
+            leftHand.position = VRRigReferences.Singleton.leftHand.position;
+            leftHand.rotation = VRRigReferences.Singleton.leftHand.rotation;
+
+            rightHand.position = VRRigReferences.Singleton.rightHand.position;
+            rightHand.rotation = VRRigReferences.Singleton.rightHand.rotation;
+        }
+
+
+    }
+}
