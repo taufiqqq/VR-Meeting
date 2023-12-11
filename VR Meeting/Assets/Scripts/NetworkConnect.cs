6423354d74f6e145ab3dad34a76a547673fcf317
Zulaikha Nazri <zulaikha02@graduate.utm.my>
2023-12-12 00:00:01 +0800

    maxPlayers

--- VR Meeting/Assets/Scripts/NetworkConnect.cs
+++ VR Meeting/Assets/Scripts/NetworkConnect.cs
@@ -9,15 +9,18 @@ using Unity.Services.Relay.Models;
 using Unity.Netcode.Transports.UTP;
 using Unity.Services.Lobbies; //rectangular notation external actor (server side)
 using Unity.Services.Lobbies.Models;
+using UnityEngine.SceneManagement;
 
 public class NetworkConnect : MonoBehaviour
-{   
-    public string lobbyId;
-    public int maxConnection = 20;
-    public UnityTransport transport;
-
+{
+    private string joinCode;
     private Lobby currentLobby;
     private float heartBeatTimer;
+
+    private int maxConnection;
+
+    //public int maxConnection = 20;
+    public UnityTransport transport;
 
     private async void Awake()
     {
@@ -28,52 +31,60 @@ public class NetworkConnect : MonoBehaviour
 
     public async void Create()
     {
-            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnection);
-            string newJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-            Debug.Log(newJoinCode);
-            Debug.LogError(newJoinCode);
+        SceneManager.LoadScene(1);
 
-            transport.SetHostRelayData(allocation.RelayServer.IpV4, (ushort)allocation.RelayServer.Port, allocation.AllocationIdBytes, allocation.Key, allocation.ConnectionData);
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnection);
+        string newJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        Debug.Log(newJoinCode);
+        Debug.LogError(newJoinCode);
 
-            CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+        transport.SetHostRelayData(allocation.RelayServer.IpV4, (ushort)allocation.RelayServer.Port, allocation.AllocationIdBytes, allocation.Key, allocation.ConnectionData);
 
-            lobbyOptions.IsPrivate = false;
+        CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
 
-            lobbyOptions.Data = new Dictionary<string, DataObject>();
-            DataObject dataObject = new DataObject(DataObject.VisibilityOptions.Public, newJoinCode);
-            lobbyOptions.Data.Add("JOIN_CODE", dataObject);
+        lobbyOptions.IsPrivate = false;
 
-            currentLobby = await Lobbies.Instance.CreateLobbyAsync("Meeting Name", maxConnection, lobbyOptions);
+        lobbyOptions.Data = new Dictionary<string, DataObject>();
+        DataObject dataObject = new DataObject(DataObject.VisibilityOptions.Public, newJoinCode);
+        lobbyOptions.Data.Add("JOIN_CODE", dataObject);
+
+        currentLobby = await Lobbies.Instance.CreateLobbyAsync("Meeting Name", maxConnection, lobbyOptions);
+
+        Debug.LogError("Lobby Code : " + currentLobby.LobbyCode);
+
+        joinCode = currentLobby.LobbyCode;
+
+        UpdateLobbyCode(joinCode);
+        NetworkManager.Singleton.StartHost();
+    }
+
+
+    public async void Join()
+    {
+        if (joinCode != "")
+        {
+            try
+            {
+                currentLobby = await Lobbies.Instance.JoinLobbyByCodeAsync(joinCode);
+                SceneManager.LoadScene(1);
+            }
+            catch (LobbyServiceException e)
+            {
+                Debug.LogError(e);
+            }
+
+            string relayJoinCode = currentLobby.Data["JOIN_CODE"].Value;
+            Debug.LogError(relayJoinCode);
+            JoinAllocation allocation = await RelayService.Instance.JoinAllocationAsync(relayJoinCode);
+
+            transport.SetClientRelayData(allocation.RelayServer.IpV4, (ushort)allocation.RelayServer.Port, allocation.AllocationIdBytes, allocation.Key, allocation.ConnectionData, allocation.HostConnectionData);
 
             Debug.LogError("Lobby ID : " + currentLobby.Id);
             Debug.LogError(currentLobby.Data["JOIN_CODE"].Value);
 
-            NetworkManager.Singleton.StartHost();
-    }
-    //Control class (client side)
-    //Combined control / boundary
-
-    public async void Join()
-    {
-        try
-        { 
-            currentLobby = await Lobbies.Instance.JoinLobbyByIdAsync(lobbyId);
+            UpdateLobbyCode(joinCode);
+            NetworkManager.Singleton.StartClient();
         }
-        catch (LobbyServiceException e)
-        {
-            Debug.LogError(e);
-        }
-
-        string relayJoinCode = currentLobby.Data["JOIN_CODE"].Value;
-        Debug.LogError(relayJoinCode);
-        JoinAllocation allocation = await RelayService.Instance.JoinAllocationAsync(relayJoinCode);
-
-        transport.SetClientRelayData(allocation.RelayServer.IpV4, (ushort)allocation.RelayServer.Port, allocation.AllocationIdBytes, allocation.Key, allocation.ConnectionData, allocation.HostConnectionData);
-        
-        Debug.LogError("Lobby ID : " + currentLobby.Id);
-        Debug.LogError(currentLobby.Data["JOIN_CODE"].Value);
-        
-        NetworkManager.Singleton.StartClient();
     }
 
     private void Update()
@@ -83,10 +94,39 @@ public class NetworkConnect : MonoBehaviour
             heartBeatTimer -= 15;
             if (currentLobby != null && currentLobby.HostId == AuthenticationService.Instance.PlayerId)
                 LobbyService.Instance.SendHeartbeatPingAsync(currentLobby.Id);
-                Debug.LogError(currentLobby.Id);
-                Debug.LogError(currentLobby.Data["JOIN_CODE"].Value);
+            Debug.LogError(currentLobby.Id);
+            Debug.LogError(currentLobby.Data["JOIN_CODE"].Value);
         }
         heartBeatTimer += Time.deltaTime;
-        
-        }
+
+    }
+
+    public void setJoinCode(string data)
+    {
+        joinCode = data;
+        // You can perform additional network-related operations if needed
+    }
+
+    public string getJoinCode()
+    {
+        return joinCode;
+    }
+    private void UpdateLobbyCode(string lobbyCode)
+    {
+        DisplayJoinCode displayJoinCode = FindObjectOfType<DisplayJoinCode>();
+        Debug.LogError("hello");
+
+        displayJoinCode.UpdateLobbyCode(lobbyCode);
+
+    }
+
+    public void setMaxConnection(int data){
+
+        maxConnection = data;
+    }
+
+    public int getMaxConnection()
+    {
+        return maxConnection;
+    }
 }
